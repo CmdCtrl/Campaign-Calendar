ade4de112f70369a3520915277e4635022d30ec1
CmdCtrl <thecmdctrl@gmail.com>
2020-09-06 18:46:43 -0400

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="../assets/banner.png" alt="">
+    <h1>Campaign Calendar</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
+
+<style scoped>
+img {
+    max-width:100%;
+    height:auto;
+}
+</style>
