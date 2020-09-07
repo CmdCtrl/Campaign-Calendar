8606911310cc16092a6107bccfcf3498d1edaa2e
CmdCtrl <thecmdctrl@gmail.com>
2020-09-07 19:15:40 -0400

    styling & clean up

--- src/components/newEntry.vue
+++ src/components/newEntry.vue
@@ -1,8 +1,6 @@
 <template>
 <div>
-<!-- 
-Create timeline component here
--->
+
 </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="home">
-    <img src="../assets/banner.png" alt="">
-    <h1>Campaign Calendar</h1>
+    <header>
+      <img src="../assets/banner.png" alt="">
+      <div class="campaign-logo">
+        <h1>Campaign</h1>
+      </div>
+      <div class="calendar-logo">
+        <h1>Calendar</h1>
+      </div>
+    </header>
+    <div class="button-container">
+      <button class="new-entry" @click="entryModal = true"><span> + </span>New Entry</button>
+      <transition name="fade" appear>
+        <div class="modal-overlay" v-if="entryModal" @click="entryModal = false">
+        </div>
+      </transition>
+      <transition name="slide" appear>
+        <div class="modal" v-if="entryModal">
+          <h1>This will be Form title</h1>
+          <p>This will be replaced with a form and input boxes. 
+            The data entered will be bound to internal variables and then displayed to the event list.
+            be sure to revisit the tutorial blog app!
+          </p>
+          <button class="new-entry" @Click="entryModal = false">THIS WILL BE SUBMIT BUTTON</button>
+
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -10,15 +35,140 @@
 
 export default {
   name: 'Home',
+  
+  data() {
+    return {
+      entryModal: true
+    };
+  },
+
   components: {
     
   }
 }
 </script>
 
-<style scoped>
-img {
-    max-width:100%;
-    height:auto;
+<style lang="scss" scoped>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
+.button-container {
+  justify-content: center;
+  align-items: center;
+  display: flex; 
+  min-height: 15vh;
+}
+
+.new-entry {
+  cursor: pointer;
+  background: none;
+  border: none;
+  outline: none;
+  appearance: none;
+
+  display: inline-block;
+  padding: 14px 22px;
+  background-image: linear-gradient(to right, #CC2e50, #FF5858);
+  border-radius: 8px;
+
+  color: #FFF;
+  font-size: 18px;
+  font-weight: 700;
+
+  box-shadow: 3px 3px rgba(0, 0, 0, 0.4);
+  transition: 0.4s ease-out;
+
+  &:hover {
+    box-shadow: 6px 6px rgba(0, 0, 0, 0.6);
+  }
+}
+.new-entry span {
+  font-size: 24px;
+  font-weight: 1200;
+}
+
+.modal-overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 98;
+  background-color: rgba(0, 0, 0, 0.3);
+}
+
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 99;
+
+  width: 100%;
+  max-width: 400px;
+  background-color: #FFF;
+  border-radius: 16px;
+  padding: 25px;
+}
+
+.modal h1 {
+  font-size: 32px;
+  color: #222;
+  font-weight: 900;
+  margin-bottom: 15px;
+}
+
+.modal p {
+  font-size: 18px;
+  font-weight: 400;
+  margin-bottom: 15px;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-active, .slide-leave-active {
+  transition: transform 0.5s;
+}
+.slide-enter, .slide-leave-to {
+  transform: translateY(-50%) translateX(100vw);
+}
+
+.campaign-logo {
+  position: absolute;
+  top: 6px;
+  left: 16px;
+  display: flex;
+  font-size: 8vh;
+  color: #FFF;
+}
+
+.calendar-logo {
+  position: absolute;
+  top: 86px;
+  left: 16px;
+  display: flex;
+  font-size: 8vh;
+  color: #FFF;
+}
+
+header img {
+    width: 100%;
+    height: 186px;
+    top: 0;
+    display: flex;
+}
+
 </style>
